de098a773e08c8ba4d10ac3e11ab3f0df555f9b3
elOrlin <orlindiaz@outlook.com>
2025-11-01 01:15:39 -0400

    Protegiendo datos sensibles.

--- simulador_pytest/settings.py
+++ simulador_pytest/settings.py
@@ -109,7 +109,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-        "PORT": os.getenv("POSTGRES_PORT", 5432),
-    }
-}
