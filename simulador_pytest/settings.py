119f775ff30651fb79938cfe2f259bd9c6f61038
elOrlin <orlindiaz@outlook.com>
2025-11-01 01:13:34 -0400

    Protegiendo datos sensibles.

--- simulador_pytest/settings.py
+++ simulador_pytest/settings.py
@@ -2,16 +2,23 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# Cargar .env
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
-# Django settings
-SECRET_KEY = os.getenv("SECRET_KEY")
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-luyg6$bhc8gp*v&bgs&imln*edqrk147&8-&zd(-9!6zs7lsb4"
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = []
 
-# Aplicaciones y middleware
+
+# Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -19,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.usuarios',
+    'apps.usuarios'
 ]
 
 MIDDLEWARE = [
@@ -33,7 +40,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'simulador_pytest.urls'
-WSGI_APPLICATION = 'simulador_pytest.wsgi.application'
 
 TEMPLATES = [
     {
@@ -50,14 +56,60 @@ TEMPLATES = [
     },
 ]
 
-# Configuración de base de datos
+WSGI_APPLICATION = 'simulador_pytest.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # 🔸 Archivo físico: db.sqlite3
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
         "PORT": os.getenv("POSTGRES_PORT", 5432),
     }
 }
