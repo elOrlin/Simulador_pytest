568247564348113b22c9e0c1e171623e18c17c4c
elOrlin <orlindiaz@outlook.com>
2025-11-01 07:06:43 -0400

    Cargando archivos estaticos

--- simulador_pytest/settings.py
+++ simulador_pytest/settings.py
@@ -6,8 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback_key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
