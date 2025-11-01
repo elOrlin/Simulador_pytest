85747ce281a8973e633a338c688b9467abe7fd48
elOrlin <orlindiaz@outlook.com>
2025-11-01 07:30:58 -0400

    Rutas de usuario

--- simulador_pytest/urls.py
+++ simulador_pytest/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 urlpatterns = [
-    path("", lambda request: HttpResponse("Hola mundo")),
-    path("admin/", admin.site.urls),
     path("api/", include("apps.usuarios.urls")),
+    path("", lambda request: HttpResponse("Hola mundo")),
+    path("admin/", admin.site.urls)
 ]
